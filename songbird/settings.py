4dd83bc7e03e8cd354b5803692c95e4184e1093f
daniarlert <arlertdaniel@gmail.com>
2023-07-12 20:14:26 +0200

    refactor: move templates to BASE_DIR

--- songbird/settings.py
+++ songbird/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
+            os.path.join(BASE_DIR, "templates"),
             os.path.join(PROJECT_DIR, "templates"),
         ],
         "APP_DIRS": True,
