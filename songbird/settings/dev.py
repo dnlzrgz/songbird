e0b4e97de0da92c18c9147ec4c73655756ac91e3
daniarlert <arlertdaniel@gmail.com>
2023-07-12 19:15:09 +0200

    feat: init project

---
+++ songbird/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-(e(d5-y=qbt$n#a5mmk#wluf@$qminxjzve_6&0qqyi^yk-^2c"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
