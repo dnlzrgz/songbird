b3b8d0109ace5a991923ecd0fc6f45fcebe323b2
daniarlert <arlertdaniel@gmail.com>
2023-07-16 23:15:01 +0200

    feat(home): add calendar, links and bio widgets models

--- home/models.py
+++ home/models.py
@@ -1,6 +1,25 @@
 from django.db import models
+from wagtail.admin.panels import FieldPanel
+from wagtail.blocks import StreamBlock
+from wagtail.fields import StreamField
 from wagtail.models import Page
+
+from .widgets import BiographyCardWidget, CalendarWidget, LinksWidget
+
+
+class Widgets(StreamBlock):
+    calendar_widget = CalendarWidget()
+    links_widget = LinksWidget()
+    biography_card_widget = BiographyCardWidget()
 
 
 class HomePage(Page):
-    pass
+    widgets = StreamField(
+        Widgets(),
+        null=True,
+        use_json_field=True,
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("widgets"),
+    ]
